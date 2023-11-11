086fed56331f04036761367ea0b1aa6b0e259701
Rron Kurtishi <rron.kurtishi@gmail.com>
2023-11-11 18:03:48 +0100

    Initial commit

---
+++ java-3/objektet/script.js
@@ -0,0 +1,14 @@
+let produkti = {
+  emri: "Telefon",
+  pershkrimi: "iPhone 13 Pro",
+  cmimi: 120
+};
+
+// Nese cmimi eshte me i madh se 100
+//    krijo nje vlere te re me 10% zbritje
+//    vendosi vleren e re me zbritje produktit
+// Perndryshe 
+//    krijo nje vlere te re me 5% zbritje
+//    vendosi vleren e re me zbritje produktit
+// Nxir ne konsole produktin me cmimin e ri
+
