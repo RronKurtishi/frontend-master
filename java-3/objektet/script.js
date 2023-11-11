9308aa94948fa2051c385d58298032f43aecac9a
Rron Kurtishi <rron.kurtishi@gmail.com>
2023-11-11 18:39:43 +0100

    Detyra me cmimin e produktit

--- java-3/objektet/script.js
+++ java-3/objektet/script.js
@@ -1,14 +1,21 @@
 let produkti = {
   emri: "Telefon",
   pershkrimi: "iPhone 13 Pro",
-  cmimi: 120
+  cmimi: 50
 };
 
 // Nese cmimi eshte me i madh se 100
-//    krijo nje vlere te re me 10% zbritje
-//    vendosi vleren e re me zbritje produktit
+if (produkti.cmimi > 100) {
+//    krijo nje vlere te re me 10% zbritje && svendosi vleren e re me zbritje produktit
+  produkti.cmimi = produkti.cmimi - (produkti.cmimi * 0.1);
+}
 // Perndryshe 
+else {
 //    krijo nje vlere te re me 5% zbritje
+  let cmimiRi = produkti.cmimi - (produkti.cmimi * 0.05)
 //    vendosi vleren e re me zbritje produktit
+  produkti.cmimi = cmimiRi;
+}
 // Nxir ne konsole produktin me cmimin e ri
+console.log(produkti);
 
