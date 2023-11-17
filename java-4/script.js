935ab05c58a3ffef7bf9ccf92c296447af583fc4
Rron Kurtishi <rron.kurtishi@gmail.com>
2023-11-17 19:19:30 +0100

    Detyra java 4

--- java-4/script.js
+++ java-4/script.js
@@ -1,6 +1,29 @@
+// ##### Objektet dhe Arrayt - Përsëritje (#1) #####
+// let produkti = {
+//  emri: "iPhone 13",
+//   pershkrimi: "iPhone 13 Pro",
+//   cmimi: 50
+// };
+
+
+// ##### For loops - Praktikë (#2) #####
+// let teNgjashme = [“rezultante”, “rezulton”, “rezolutë”, “rezil”, “rezmishta”, “rezervuar”, “reze”]
+
+
+// ##### For loops - Praktikë (#3) #####
+
+
+// ##### Array Iteration: Praktike (#4) #####
 // const studentet = ['Aldin', 'Ali', 'Almir', 'Arlind', 'Artan', 'Elvedin', 'Gafar', 'Jon'];
 
 
+
+
+// ##### While Loop - Praktike (#5) #####
+
+
+
+// ##### While Loop - Praktike (#6) #####
 // nderkohe qe numri nuk eshte i sakte
     // printo ne konzole mesazhin per te provuar perseri
     // shfaq prompt-in perseri
