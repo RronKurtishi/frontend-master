91cae4c7fdae4597a14c5264d57ece258ea77422
Rron Kurtishi <rron.kurtishi@gmail.com>
2023-11-17 19:12:49 +0100

    Detyrat e javes 4

---
+++ java-4/script.js
@@ -0,0 +1,8 @@
+// const studentet = ['Aldin', 'Ali', 'Almir', 'Arlind', 'Artan', 'Elvedin', 'Gafar', 'Jon'];
+
+
+// nderkohe qe numri nuk eshte i sakte
+    // printo ne konzole mesazhin per te provuar perseri
+    // shfaq prompt-in perseri
+// perndryshe
+    // printo ne konsole mesazhin e suksesit
