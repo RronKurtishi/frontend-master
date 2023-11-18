bc6d728128e04265b92323eaf7d9494cbceb98fb
Rron Kurtishi <rron.kurtishi@gmail.com>
2023-11-18 17:32:28 +0100

    Detyrat java 4 ora 1

--- java-4/script.js
+++ java-4/script.js
@@ -2,22 +2,31 @@
 // let produkti = {
 //  emri: "iPhone 13",
 //   pershkrimi: "iPhone 13 Pro",
-//   cmimi: 50
+//   cmimi: 50,
+//   tags: ['telefon', 'iphone', 'apple']
 // };
+
+// produkti.tags = ['telefon', 'iphone', 'apple'];
+
+// console.log(produkti.tags[0]);
+// console.log(produkti.tags[1]);
+// console.log(produkti.tags[2]);
 
 
 // ##### For loops - Praktikë (#2) #####
-// let teNgjashme = [“rezultante”, “rezulton”, “rezolutë”, “rezil”, “rezmishta”, “rezervuar”, “reze”]
+let teNgjashme = ["rezultante", "rezulton", "rezolutë", "rezil", "rezmishta", "rezervuar", "reze"];
 
+for (let i = 0; i < teNgjashme.length; i++) {
+    console.log(teNgjashme[i])
+}
 
 // ##### For loops - Praktikë (#3) #####
 
 
 // ##### Array Iteration: Praktike (#4) #####
-// const studentet = ['Aldin', 'Ali', 'Almir', 'Arlind', 'Artan', 'Elvedin', 'Gafar', 'Jon'];
+const studentet = ['Aldin', 'Ali', 'Almir', 'Arlind', 'Artan', 'Elvedin', 'Gafar', 'Jon'];
 
-
-
+studentet[0]
 
 // ##### While Loop - Praktike (#5) #####
 
