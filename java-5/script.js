a9329b05ffb2c8b11c8bd58b1524ad245bb7090a
Rron Kurtishi <rron.kurtishi@gmail.com>
2023-11-24 17:11:41 +0100

    Java 5, dita 1

---
+++ java-5/script.js
@@ -0,0 +1,13 @@
+// ##### Loops - Përsëritje (#1) #####
+// let arr1 = [3, 4, 9, 25,  7,  42,  13, 5, 8, 85, 55];
+// let arr2 = [5, 25, 44, 3, 7, 62, 9, 12, 20, 36, 76]; 
+
+// ##### Funksionet - Praktikë (#2)) #####
+
+// #### Funksionet me Parametra - Praktikë (#3) #####
+
+// ##### Funksionet me Parametra - Praktikë (#4) #####
+
+// ##### Return - Praktikë (#5) #####
+
+// ##### Funksionet - Praktikë (#6) #####
