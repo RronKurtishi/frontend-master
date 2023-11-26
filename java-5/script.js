daa9abe75bf86c69bec3a648e2b2fc7bed230921
Rron Kurtishi <rron.kurtishi@gmail.com>
2023-11-26 15:25:58 +0100

    Detyrat java 5, dita 2

--- java-5/script.js
+++ java-5/script.js
@@ -39,29 +39,66 @@ const prodhimiGlobal = prodhimiINumrave(2, 20);
 console.log(prodhimiGlobal);
 
 // ##### Funksionet me Parametra - Praktikë (#4) #####
+function printoStringun(teksti, numri) {
+  for (i = 0; i < numri; i++) {
+    console.log(teksti);
+  }
+}
 
+printoStringun("Tekst", 5);
+printoStringun("Rron", 20);
 // ##### Funksionet - Praktikë (#6) #####
+function nenZero(numri) {
+  if (numri < 0) {
+    return true;
+  } else {
+    return false
+  }
+  // aletrnative e if/else
+  // return numri < 0;
+}
+const ngrinUji = nenZero(55);
 
 // ##### Funksionet me Shigjetë - Praktikë (#7) #####
+const prodhimiINumrave = function (num1, num2) {
+  const prodhimi = num1 * num2;
+  console.log(prodhimi);
+}
+
+// const prodhimiINumrave = (num1, num2) => {
+//   const prodhimi = num1 * num2;
+//   console.log(prodhimi);
+// }
 
 // ##### forEach() - Praktikë (#8) #####
 let produktet = [{
   emri: 'Xbox Series S',
   cmimi: 20600
-  }, {
+}, {
   emri: 'Play Station 5',
   cmimi: 28000
-  }, {
+}, {
   emri: 'Play Station 4',
   cmimi: 21000
-  }, {
+}, {
   emri: 'Nintendo Switch',
   cmimi: 25000
-  }, {
+}, {
   emri: 'Quest 2',
   cmimi: 30000
-  }]; 
-  
+}];
+
+produktet.forEach((p) => {
+  console.log(p.emri)
+})
+
 // ##### map() - Praktikë (#9) #####
+const meZbritje = produktet.map((p) => {
+  p.cmimiMeZbritje = p.cmimi * 0.8;
+  return p;
+});
 
 // ##### filter() - Praktikë (#10) #####
+const teFiltruara = meZbritje.filter((p) => {
+  return p.cmimiMeZbritje < 21000;
+})
